afdb534f70a7383ad469b1471b9b785ea4366ec5
Will Haack <wmh1993@gmail.com>
2014-05-05 23:31:06 -0400

    now actually lists sites

--- pownews/pownews/views.py
+++ pownews/pownews/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from pownews.models import *
+from django.template import RequestContext
+from hashlib import sha256
 
 def index(request):
     links = LinkEntry.objects.order_by('hashed')[0:10]
-    return render_to_response("index.html", links)
+    formattedLinks = formatLinks(links)
+    return render_to_response("index.html", {'links': formattedLinks},
+      context_instance=RequestContext(request))
+
+def formatLinks(queryLinks):
+    formattedLinks = []
+    for link in queryLinks:
+      formattedLink = {}
+      formattedLink['link'] = link.link
+      formattedLink['nonce'] = link.nonce
+      formattedLink['hash'] = link.hashed
+      formattedLinks.append(formattedLink)
+    return formattedLinks
+
+def addLink(request):
+  link = request.POST['link']
+  nonce = request.POST['nonce']
+  hashed = sha256(link + nonce).hexdigest()
+  
